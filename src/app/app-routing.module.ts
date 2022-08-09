d47eb47daa1e86b269000e5fff6bd7a031c03691
Thang <thangnhth2202028@fpt.edu.vn>
2022-08-09 21:21:36 +0700

    upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,10 @@ import {AboutComponent} from "./about/about.component";
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  { path : 'home-component', component: HomeComponent},
-  { path : 'shop-component', component: ShopComponent},
-  { path : 'about-component', component : AboutComponent},
-  { path : 'contact-component', component : ContactComponent}
+  { path : 'home', component: HomeComponent},
+  { path : 'shop', component: ShopComponent},
+  { path : 'about', component : AboutComponent},
+  { path : 'contact', component : ContactComponent}
 ];
 
 @NgModule({
